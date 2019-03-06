74906cc7078b5282406f2cd33c8a06ea09acdcbc
MSI\ben <ben@presentcreative.com>
2019-03-06 02:53:35 -0800

    switched to switch statements

--- MidiJack-master/Assets/Example CC/KnobIndicatorGroup.cs
+++ MidiJack-master/Assets/Example CC/KnobIndicatorGroup.cs
@@ -12,32 +12,20 @@ public class KnobIndicatorGroup : MonoBehaviour
 
     //ben variables
     public float midiParticleStartSize;
-    public float midiParticleStartSizePrev;
-
     public float midiParticleStartSpeed;
-    public float midiParticleStartSpeedPrev;
-
     public float midiParticleEmissionRate;
-    public float midiParticleEmissionRatePrev;
-
     public float midiParticleGravityModifier;
-    public float midiParticleGravityModifierPrev;
-
     public float midiParticleStartLifetime;
-    public float midiParticleStartLifetimePrev;
-
     public float midiParticleforceOverLifetime;
-    public float midiParticleforceOverLifetimePrev;
-
     public float midiParticleDampen;
-    public float midiParticleDampenPrev;
-
     public float midiParticleLifetime;
-    public float midiParticleLifetimePrev;
+    public float midiParticleShapeAngle;
 
-    public List<int> midiWokeKnobs; // the midi CC number
-   // public List<float> midiValuesList; // the midi CC number
-    //public List<float> midiConnectors;  // for the actual midi flaot values
+   
+
+    public List<int> midiWokeKnobs; // a list of the the midi CC numbers created in the order they were touched
+    public List<float> midiKnobVal; // a list of the values come from those knobs in the same order
+    public List<float> midiKnobValPrev;
 
     void Start()
     {
@@ -49,11 +37,11 @@ public class KnobIndicatorGroup : MonoBehaviour
         
         var channels = MidiMaster.GetKnobNumbers();
 
-        // If a new chennel was added...
+        // If a new channel was added...
         if (indicators.Count != channels.Length)
         {
             // Instantiate the new indicator.
-            var go = Instantiate<GameObject>(prefab);
+            var go = Instantiate<GameObject>(prefab);  // this is for the bars for each new midi knob
             go.transform.position = Vector3.right * indicators.Count;
 
             // Initialize the indicator.
@@ -64,113 +52,135 @@ public class KnobIndicatorGroup : MonoBehaviour
             indicators.Add(indicator);
             //ben adds to his list
             midiWokeKnobs.Add(indicator.knobNumber);
+           midiKnobVal.Add(MidiMaster.GetKnob(midiWokeKnobs.Count));
+           midiKnobValPrev.Add(MidiMaster.GetKnob(midiWokeKnobs.Count));
             //midiConnectors.Add(midiWokeKnobs[indicator.Count]);
             //midiConnectors[indicators.Count] = midiWokeKnobs[indicators.Count];
         }
 
         //ben section
-        //var s = MidiMaster.GetKnob(knobNumber);
-        // MidiVal1 = MidiMaster.GetKnob(midiWokeKnobs[0]); // this version works for loading in the midiWokeKnobs as they get created
-        //MidiVal0 = MidiMaster.GetKnob(0);
-        /*if (midiWokeKnobs.Count > 0) {
-            midiVal1 = Map(0, 20, 0, 1, MidiMaster.GetKnob(midiWokeKnobs[1]));
-        }
-        */
-        /*
-        midiVal2 = Map(0, 300, 0, 1, MidiMaster.GetKnob(midiWokeKnobs[2]));
-        midiVal3 = Map(0, 200, 0, 1, MidiMaster.GetKnob(midiWokeKnobs[3]));
-        midiVal4 = Map(0, 200, 0, 1, MidiMaster.GetKnob(midiWokeKnobs[4]));
-        midiVal5 = Map(0, 10, 0, 1, MidiMaster.GetKnob(midiWokeKnobs[5]));
-        midiVal6 = Map(0, 255, 0, 1, MidiMaster.GetKnob(midiWokeKnobs[6]));
-        midiVal7 = Map(0, 255, 0, 1, MidiMaster.GetKnob(midiWokeKnobs[7]));
-        */
+        /* foreach (int i in midiKnobVal)
+         {
+             int midiI = i + 1;
+             print(i);
+             midiKnobVal[i] = Map(0, 300, 0, 1, MidiMaster.GetKnob(midiI));
+         }
+         */
 
-        /*
-        midiParticleSystem.startSize = midiVal1;
-        midiParticleSystem.startSpeed = midiVal2;
-        midiParticleSystem.emissionRate = midiVal3;
-        midiParticleSystem.gravityModifier = midiVal4;
-        midiParticleSystem.startLifetime = midiVal5;
-        */
-
-        //STARTSIZE
-        midiParticleStartSize = Map(0, 300, 0, 1, MidiMaster.GetKnob(1));
-        if (midiParticleStartSize != midiParticleStartSizePrev)
+        for (int i = 0; i < midiKnobVal.Count; i++)
         {
-            
-            midiParticleSystem.startSize = midiParticleStartSize;
-            midiParticleStartSizePrev = midiParticleStartSize;
+            Debug.Log("for statement things the midiKnobVal Count is " + midiKnobVal.Count + "and the current number is " + i);
+            int midiI = i + 1;
+            midiKnobVal[i] = MidiMaster.GetKnob(midiI);
+            if(midiKnobValPrev[i] != midiKnobVal[i])
+            {
+                
+                
+                switch (i)
+                {
+                    case 7:
+                        ShapeAngle(midiKnobVal[i]);
+                        break;
+                    case 6:
+                        Dampen(midiKnobVal[i]);
+                        break;
+                    case 5:
+                        print("5");
+                        ForceOverLifeTime(midiKnobVal[i]);
+                        break;
+                    case 4:
+                        print("4");
+                        StartLifeTime(midiKnobVal[i]);
+                        break;
+                    case 3:
+                        print("3");
+                        GravityModifier(midiKnobVal[i]);
+                        break;
+                    case 2:
+                        print("2");
+                        EmissionRate(midiKnobVal[i]);
+                        break;
+                    case 1:
+                        print("1 start size");
+                        StartSpeed(midiKnobVal[i]);
+                        break;
+                    default:
+                        print("start size");
+                        StartSize(midiKnobVal[i]);
+                        break;
+                }
+                midiKnobValPrev[i] = midiKnobVal[i];
+            }
+            //Debug.Log("midiKnobVale COunt is " + i);
         }
-
-        //STARTSPEED
-        midiParticleStartSpeed = Map(-400, 400, 0, 1, MidiMaster.GetKnob(2));
-        if (midiParticleStartSpeed != midiParticleStartSpeedPrev)
-        {
-            
-            midiParticleSystem.startSpeed = midiParticleStartSpeed;
-            midiParticleStartSpeedPrev = midiParticleStartSpeed;
-        }
-
-        //EMISSION RATE
-        midiParticleEmissionRate = Map(0, 500, 0, 1, MidiMaster.GetKnob(3));
-        if (midiParticleEmissionRate != midiParticleEmissionRatePrev)
-        {
-            midiParticleSystem.emissionRate = midiParticleEmissionRate;
-            midiParticleEmissionRatePrev = midiParticleEmissionRate;
-        }
-
-        //GRAVITY MODIFIER
-        midiParticleGravityModifier = Map(0, 500, 0, 1, MidiMaster.GetKnob(4));
-        if (midiParticleGravityModifier != midiParticleGravityModifierPrev)
-        {
-            midiParticleSystem.gravityModifier = midiParticleGravityModifier;
-            midiParticleGravityModifierPrev = midiParticleGravityModifier;
-        }
-
-        //STARTLIFETIME
-        midiParticleLifetime = Map(0, 20, 0, 1, MidiMaster.GetKnob(5));
-        if (midiParticleLifetime != midiParticleLifetimePrev)
-        {
-            midiParticleSystem.startLifetime = midiParticleLifetime;
-            midiParticleLifetimePrev = midiParticleLifetime;
-        }
-        //FORCE OVER LIFETIME
-        midiParticleforceOverLifetime = Map(0, 500, 0, 1, MidiMaster.GetKnob(6));
-        //version for setting an attribute that is a STRUCT https://blogs.unity3d.com/2016/04/20/particle-system-modules-faq/
-        if (midiParticleforceOverLifetime != midiParticleforceOverLifetimePrev) {
-            
-            var forceModule = midiParticleSystem.forceOverLifetime;
-            // Enable it and set a value
-            //forceModule.enabled = true;
-            forceModule.y = new ParticleSystem.MinMaxCurve(midiParticleforceOverLifetime);
-            midiParticleforceOverLifetimePrev = midiParticleforceOverLifetime;
-        }
-
-        //DAMPEN
-        midiParticleDampen = Map(0, 1, 0, 1, MidiMaster.GetKnob(81));
-        //version for setting an attribute that is a STRUCT https://blogs.unity3d.com/2016/04/20/particle-system-modules-faq/
-        if (midiParticleDampen != midiParticleDampenPrev)
-        { 
-            var limitVelocityModule = midiParticleSystem.limitVelocityOverLifetime;
-            // Enable it and set a value
-            //limitVelocityModule.enabled = true;
-            limitVelocityModule.dampen = midiParticleDampen;
-            midiParticleDampenPrev = midiParticleDampen;
-        }
-
-
         if (Input.GetButton("Fire1"))
         {
             //Debug.Log(midiParticleSystem.main.simulationSpeed);
-            //var forceModule = GetComponent<ParticleSystem>().forceOverLifetime;
-            var forceModule = midiParticleSystem.forceOverLifetime;
-
-            // Enable it and set a value
-            forceModule.enabled = true;
-            forceModule.x = new ParticleSystem.MinMaxCurve(15.0f);
+   //         StartSize();
         }
 
     }
+
+    //CHANGE KNOB ASSIGNMENT
+    //click button
+    //touch knob assigns knob to button
+    //
+
+    void StartSize(float passedMidiValue)
+    {
+        midiParticleStartSize = Map(0, 300, 0, 1, passedMidiValue);
+        midiParticleSystem.startSize = midiParticleStartSize;
+       // }
+    }
+    void StartSpeed(float passedMidiValue)
+    {
+        midiParticleStartSpeed = Map(-500, 500, 0, 1, passedMidiValue);
+        midiParticleSystem.startSpeed = midiParticleStartSpeed;
+        // }
+    }
+    void EmissionRate(float passedMidiValue)
+    {
+        midiParticleEmissionRate = Map(0, 500, 0, 1, passedMidiValue);
+        midiParticleSystem.emissionRate = midiParticleEmissionRate;
+        // }
+    }
+    void GravityModifier(float passedMidiValue)
+    {
+        midiParticleGravityModifier = Map(0, 500, 0, 1, passedMidiValue);
+        midiParticleSystem.gravityModifier = midiParticleGravityModifier;
+        // }
+    }
+    void StartLifeTime(float passedMidiValue)
+    {
+        midiParticleLifetime = Map(0, 15, 0, 1, passedMidiValue);
+        midiParticleSystem.startLifetime = midiParticleLifetime;
+        // }
+    }
+    void Dampen(float passedMidiValue)
+    {
+        midiParticleDampen = Map(0, 1, 0, 1, passedMidiValue);
+        var limitVelocityModule = midiParticleSystem.limitVelocityOverLifetime;
+        limitVelocityModule.enabled = true;
+        limitVelocityModule.dampen = midiParticleDampen;
+    }
+    void ShapeAngle(float passedMidiValue)
+    {
+        midiParticleShapeAngle = Map(0, 500, 0, 1, passedMidiValue);
+        var limitVelocityModule = midiParticleSystem.shape;
+        // Enable it and set a value
+        //limitVelocityModule.enabled = true;
+        limitVelocityModule.angle = midiParticleShapeAngle;
+    }
+    void ForceOverLifeTime(float passedMidiValue)
+    {
+        //version for setting an attribute that is a STRUCT https://blogs.unity3d.com/2016/04/20/particle-system-modules-faq/
+        midiParticleforceOverLifetime = Map(0, 500, 0, 1, passedMidiValue);
+        var forceModule = midiParticleSystem.forceOverLifetime;
+        // Enable it and set a value
+        //forceModule.enabled = true;
+        forceModule.y = new ParticleSystem.MinMaxCurve(midiParticleforceOverLifetime);
+    }
+
 
     // used to normalize any value range (new min, new max, old min, old max, oldValue)
     public float Map(float from, float to, float from2, float to2, float value)
